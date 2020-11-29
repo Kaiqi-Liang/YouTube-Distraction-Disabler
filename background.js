c49dc060a412534d92c727d55cead0608221d7f2
Kaiqi Liang <kaiqi.liang@outlook.com>
2020-11-29 19:35:20 +1100

    Use chrome storage

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
-let home = true;
-let related = true;
-let comments = true;
+chrome.storage.sync.set({
+    home: true,
+    related: true,
+    comments: true,
+});
 
 ['home', 'related', 'comments'].forEach((context) => {
     chrome.contextMenus.create({
@@ -8,24 +10,26 @@ let comments = true;
         title: context,
         type: 'checkbox',
         checked: true,
-        contexts: ['page_action'],
+        contexts: ['page_action'], // click anywhere on the page including any YouTube videos and any tabs
     });
 });
 
 const sendMessage = (id) => {
-    chrome.tabs.sendMessage(id, { home, related, comments });
+    chrome.storage.sync.get(['home', 'related', 'comments'], ({ home, related, comments }) => {
+        chrome.tabs.sendMessage(id, { home, related, comments });
+    });
 };
 
 chrome.contextMenus.onClicked.addListener(({ menuItemId, checked }, tab) => {
     switch (menuItemId) {
         case 'home':
-            home = checked;
+            chrome.storage.sync.set({ home: checked });
             break;
         case 'related':
-            related = checked;
+            chrome.storage.sync.set({ related: checked });
             break;
         case 'comments':
-            comments = checked;
+            chrome.storage.sync.set({ comments: checked });
             break;
         default:
             break;
